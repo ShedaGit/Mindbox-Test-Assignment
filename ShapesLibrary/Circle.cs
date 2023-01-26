039ad510b634a6773a5b6a3279d9a3a5f5a5e389
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-01-27 02:46:42 +0300

    add Circle class w/ realization

---
+++ ShapesLibrary/Circle.cs
@@ -0,0 +1,15 @@
+﻿namespace ShapesLibrary;
+public class Circle : IShape
+{
+    private readonly double _radius;
+    
+    public Circle(double radius)
+    {
+        this._radius = radius;
+    }
+
+    public double Area()
+    {
+        return Math.PI * Math.Pow(_radius, 2);
+    }
+}
